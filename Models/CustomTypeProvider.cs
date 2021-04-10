f9b1e348526e0a47e2e2da5dac739269e9f64aa5
Liam Goldfinch <liamwoody323@gmail.com>
2021-04-10 23:59:42 +0100

    added base statiq setup, added initial model from Kontent

---
+++ Models/CustomTypeProvider.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Kentico.Kontent.Delivery.Abstractions;
+
+namespace Goldfinch.Models
+{
+    public class CustomTypeProvider : ITypeProvider
+    {
+        private static readonly Dictionary<Type, string> _codenames = new Dictionary<Type, string>
+        {
+            {typeof(BlogDetail), "blog_detail"},
+            {typeof(BlogListing), "blog_listing"},
+            {typeof(CodeBlock), "code_block"},
+            {typeof(Home), "home"},
+            //{typeof(Image), "image"}
+        };
+
+        public Type GetType(string contentType)
+        {
+            return _codenames.Keys.FirstOrDefault(type => GetCodename(type).Equals(contentType));
+        }
+
+        public string GetCodename(Type contentType)
+        {
+            return _codenames.TryGetValue(contentType, out var codename) ? codename : null;
+        }
+    }
+}
---
+++ Pipelines/HomePipeline.cs
@@ -0,0 +1,51 @@
+﻿using Goldfinch.Models;
+using Kentico.Kontent.Delivery.Abstractions;
+using Kentico.Kontent.Delivery.Urls.QueryParameters;
+using Kontent.Statiq;
+using Statiq.Common;
+using Statiq.Core;
+using Statiq.Razor;
+
+namespace Goldfinch.Pipelines
+{
+    public class HomePipeline : Pipeline
+    {
+        public HomePipeline(IDeliveryClient client)
+        {
+            InputModules = new ModuleList
+            {
+                // Load "Home" item and transfer it into IDocument.
+                // <see href="https://github.com/alanta/Kontent.Statiq">Kontent.Statiq</see>
+                new Kontent<Home>(client)
+                    .WithQuery(
+                        new LimitParameter(1)
+                    ),
+                // Load Razor template to IDocument content.
+                // <see href="https://github.com/statiqdev/Statiq.Framework/blob/main/src/core/Statiq.Core/Modules/Content/MergeContent.cs">MergeContent</see>.
+                // <see href="https://statiq.dev/web/content-and-data/content/">Content propery of IDocument</see>
+                new MergeContent(
+                    new ReadFiles(patterns: "_Home.cshtml")
+                )
+            };
+
+            ProcessModules = new ModuleList
+            {
+                // Render HTML file from Razor template and document typed as Hero model.
+                // <see href="https://github.com/statiqdev/Statiq.Framework/blob/main/src/extensions/Statiq.Razor/RenderRazor.cs">RenderRazor</see>
+                new RenderRazor()
+                    .WithModel(KontentConfig.As<Home>()),
+
+                // Set file system destionation for the document.
+                // <see href="https://github.com/statiqdev/Statiq.Framework/blob/main/src/core/Statiq.Core/Modules/IO/SetDestination.cs">SetDestination</see>
+                new SetDestination(new NormalizedPath("index.html")),
+            };
+
+            OutputModules = new ModuleList
+            {
+                // Flush the the output.
+                // <see href="https://github.com/statiqdev/Statiq.Framework/blob/main/src/core/Statiq.Core/Modules/IO/WriteFiles.cs">WriteFiles</see>.
+                new WriteFiles()
+            };
+        }
+    }
+}
