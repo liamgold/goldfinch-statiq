dfeac5185e736654d994238f5b3b40d9a2f39417
Liam Goldfinch <liamwoody323@gmail.com>
2021-06-12 21:11:55 +0100

    Add sitemap

---
+++ Pipelines/SiteMapPipeline.cs
@@ -0,0 +1,52 @@
+﻿using Kontent.Statiq;
+using Statiq.Common;
+using Statiq.Core;
+using System;
+using System.Linq;
+
+namespace Goldfinch.Pipelines
+{
+    public class SiteMapPipeline : Pipeline
+    {
+        public SiteMapPipeline()
+        {
+            Dependencies.AddRange(
+                nameof(BlogDetailPipeline),
+                nameof(BlogListingPipeline),
+                nameof(HomePipeline)
+            );
+
+            ProcessModules = new ModuleList
+            {
+                new ReplaceDocuments(Dependencies.ToArray()),
+
+                new SetMetadata(Keys.SitemapItem, Config.FromDocument((doc, ctx) =>
+                {
+                    var siteMapItem = new SitemapItem(doc.Destination.FullPath)
+                    {
+                        LastModUtc = doc.Get<DateTime?>(KontentKeys.System.LastModified, null)
+                    };
+
+                    if (!siteMapItem.LastModUtc.HasValue)
+                    {
+                        siteMapItem.LastModUtc = DateTime.UtcNow;
+                        siteMapItem.ChangeFrequency = SitemapChangeFrequency.Weekly;
+                    }
+                    else
+                    {
+                        siteMapItem.ChangeFrequency = SitemapChangeFrequency.Monthly;
+                    }
+
+                    return siteMapItem;
+                })),
+
+                new GenerateSitemap(),
+            };
+
+            OutputModules = new ModuleList
+            {
+                new WriteFiles(),
+            };
+        }
+    }
+}
